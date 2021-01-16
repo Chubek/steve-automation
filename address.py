9928a1a8d40324f0d9dfc3f39507b5507cc32409
Chubak <chubakbidpaa@gmail.com>
2021-01-16 10:08:43 +0330

    new stuff

--- address.py
+++ address.py
@@ -1,1 +1,32 @@
+import googlemaps
+
 class Address:
+
+    def __init__(self, name, soc, address, city, province, zip):
+        self.name = name
+        self.soc = soc
+        self.address = address
+        self.city = city
+        self.province = province
+        self.zip = zip
+
+
+    def __get_geocode(self):
+        KEY = 'AIzaSyABRC1BA_smOa1N8pBjP-Pvco4BNqb2l_s'
+
+        gmaps = googlemaps.Client(key=KEY)
+        
+        gcode = gmaps.geocode(f"{self.address}, {self.city}, {self.province}")
+        lat_long = gcode[0]["geometry"]["location"]
+
+        return lat_long["lat"], lat_long["lng"]
+
+    def __hash_zip_soc(self):
+        return hash(self.zip), hash(self.soc)
+
+
+    def return_val(self):
+        lat, lng = self.__get_geocode()
+        zip_hash, soc_hash = self.__hash_zip_soc()
+
+        return (lat, lng, zip_hash, soc_hash)
---
+++ cluster.py
@@ -0,0 +1,18 @@
+from sklearn.cluster import KMeans
+
+
+def cluster(X, X_prime=[]):
+    km = KMeans(
+        n_clusters=3, init='random',
+        n_init=10, max_iter=300, 
+        tol=1e-04, random_state=0
+    )
+
+    km.fit(X)
+
+    new = None
+
+    if X_prime:
+        new = km.fit_predict(X_prime)
+
+    return km.labels_, new
